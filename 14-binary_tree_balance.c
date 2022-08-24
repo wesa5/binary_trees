4525450393bb94cee944178505e7ff1adff5e30c
wesa5 <wobulufrancis@gmail.com>
2022-08-24 18:11:29 +0300

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,54 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measure the balance factor
- * of a binary tree
- * @tree: pointer to root node of tree to measure
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
  *
- * Return: balance factor left height minus right height,
- * or 0 if tree is NULL
+ * Return: If tree is NULL, return 0, else return balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-	return (left - right);
+	return (0);
 }
 
 /**
- * custom_binary_tree_height - Measure the height
- * of a binary tree from a given node
- * @tree: pointer to node of tree to measure
- * Description: Edited to work with balance factor function
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
  *
- * Return: height of tree or -1 if NULL
+ * Return: If tree is NULL, your function must return 0, else return height.
  */
-int custom_binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left, right;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-	{
-		return (-1);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
 	}
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
-	left = custom_binary_tree_height(tree->left) + 1;
-	right = custom_binary_tree_height(tree->right) + 1;
-	if (left > right)
-	{
-		return (left);
-	}
-	else
-	{
-		return (right);
-	}
+	return (0);
 }
